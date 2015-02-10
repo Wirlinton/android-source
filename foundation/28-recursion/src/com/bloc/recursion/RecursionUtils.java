09a2a348e649869c34e96d8280795933a141304e
Andre Wilson <biscuitsmagee@gmail.com>
2015-02-10 18:12:00 +1100

    Checkpoint 28 completed

--- foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
+++ foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -13,13 +13,32 @@ public class RecursionUtils extends Object {
 	 * @return the largest number in the list
 	 *
 	 * Hint: your base case may be a comparison of 2 numbers
+	 * 
 	 */
+	
+	
 	public static final int findMaxRecursively(List<Integer> numbers) {
+
+		if(numbers.size() == 1) {
+			return numbers.get(0);
+		}
+		Integer valueOne = numbers.get(0);
+		Integer valueTwo = numbers.get(1);
+		if(valueOne > valueTwo) {
+			numbers.remove(1);
+		}
+		else {
+			numbers.remove(0);
+		}
+		
+		return findMaxRecursively(numbers);
+
+		
 		/************************************************
 	 	 *	ASSIGNMENT:
 	 	 *	Implement this method, the return value must
 	 	 *	change
 		/************************************************/
-		return 0;
-	}
-}
+ }
+	
+}	

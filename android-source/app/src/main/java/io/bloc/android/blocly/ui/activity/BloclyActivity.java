8cd363dfc0eebed3e3fd78c69d2d0028445f5607
Andre Wilson <biscuitsmagee@gmail.com>
2015-02-19 19:20:54 +1100

    Checkpoint 35 assignment completed

--- android-source/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ android-source/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,23 +2,30 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
-import io.bloc.android.blocly.io.bloc.android.blocly.BloclyApplication;
 
 /**
  * Created by Andre on 16/02/2015.
  */
 public class BloclyActivity extends Activity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+        TextView textElement = (TextView) findViewById(R.id.textViewId);
+        textElement.setText("My Favorite Feed");
+
             Toast.makeText(this,
                     BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
                     Toast.LENGTH_LONG).show();
     }
+
 
 }

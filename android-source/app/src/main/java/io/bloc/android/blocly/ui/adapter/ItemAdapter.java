df0f7acc263f237355f14c65d2588fdd65dd7df1
Andre Wilson <biscuitsmagee@gmail.com>
2015-02-23 14:50:42 +1100

    Checkpoint 38 completed

--- android-source/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ android-source/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -1,10 +1,17 @@
 package io.bloc.android.blocly.ui.adapter;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
@@ -18,6 +25,7 @@ import io.bloc.android.blocly.api.model.RssItem;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
 
+    private static String TAG = ItemAdapter.class.getSimpleName();
     // #6
     @Override
     public ItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
@@ -39,24 +47,63 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
     }
 
     // #9
-    class ItemAdapterViewHolder extends RecyclerView.ViewHolder {
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener {
 
         TextView title;
         TextView feed;
         TextView content;
+        View headerWrapper;
+        ImageView headerImage;
+        // #6
+        String imageURL;
 
         public ItemAdapterViewHolder(View itemView) {
             super(itemView);
-// #10
             title = (TextView) itemView.findViewById(R.id.tv_rss_item_title);
             feed = (TextView) itemView.findViewById(R.id.tv_rss_item_feed_title);
             content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
+// #7
+            headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
+            headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
         }
 
         void update(RssFeed rssFeed, RssItem rssItem) {
             feed.setText(rssFeed.getTitle());
             title.setText(rssItem.getTitle());
             content.setText(rssItem.getDescription());
+            imageURL = rssItem.getImageUrl();
+            if (imageURL != null) {
+// #8
+                headerWrapper.setVisibility(View.VISIBLE);
+                headerImage.setVisibility(View.INVISIBLE);
+                ImageLoader.getInstance().loadImage(imageURL, this);
+            } else {
+// #9
+                headerWrapper.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onLoadingStarted(String imageUri, View view) {}
+
+        @Override
+        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+            Log.e(TAG, "OnLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+        }
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+// #10
+            if (imageUri.equals(imageURL)) {
+                headerImage.setImageBitmap(loadedImage);
+                headerImage.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onLoadingCancelled(String imageUri, View view) {
+            // Attempt a retry
+            ImageLoader.getInstance().loadImage(imageUri, this);
         }
     }
 }

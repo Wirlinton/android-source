deba318debbfca874dda078a986edb9b05ece517
Andre Wilson <biscuitsmagee@gmail.com>
2015-03-04 18:57:22 +1100

    checkpoint 44 completed

--- android-source/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ android-source/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
+import io.bloc.android.blocly.ui.adapter.NavigationDrawerAdapter;
 
 /**
  * Created by Andre on 16/02/2015.
@@ -20,9 +21,10 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 public class BloclyActivity extends ActionBarActivity {
 
 
-private ItemAdapter itemAdapter;
+    private ItemAdapter itemAdapter;
     private ActionBarDrawerToggle drawerToggle;
     private DrawerLayout drawerLayout;
+    private NavigationDrawerAdapter navigationDrawerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,12 @@ private ItemAdapter itemAdapter;
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
         drawerLayout.setDrawerListener(drawerToggle);
+
+        navigationDrawerAdapter = new NavigationDrawerAdapter();
+        RecyclerView navigationRecyclerView = (RecyclerView) findViewById(R.id.rv_nav_activity_blocly);
+        navigationRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        navigationRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        navigationRecyclerView.setAdapter(navigationDrawerAdapter);
     }
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
